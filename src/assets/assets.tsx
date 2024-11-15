00ee124cd4f36a70bfc54968f9cc5d92927e87e8
Reza F <rezarffahlevi@gmail.com>
2024-11-15 23:22:19 +0700

    feat(setup): add assets icon

---
+++ src/assets/assets.tsx
@@ -0,0 +1,8 @@
+
+
+export const AppAssets = {
+    ic_search: require('./icons/ic_search.png'),
+    ic_copy: require('./icons/ic_copy.png'),
+    ic_right: require('./icons/ic_right.png'),
+    ic_arrow_down: require('./icons/ic_arrow_down.png'),
+}
